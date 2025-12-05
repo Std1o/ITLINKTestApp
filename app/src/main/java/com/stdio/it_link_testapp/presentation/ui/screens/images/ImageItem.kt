2277e3d90ec37468dea7b6f8207c0222b10d47ec
Std1o <37378410+Std1o@users.noreply.github.com>
2025-12-05 21:50:22 +0300

    error with placeholder

--- app/src/main/java/com/stdio/it_link_testapp/presentation/ui/screens/images/ImageItem.kt
+++ app/src/main/java/com/stdio/it_link_testapp/presentation/ui/screens/images/ImageItem.kt
@@ -1,32 +1,48 @@
 package com.stdio.it_link_testapp.presentation.ui.screens.images
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Error
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.stdio.it_link_testapp.R
 import com.stdio.it_link_testapp.domain.model.ImageData
 import com.stdio.it_link_testapp.domain.model.LoadableData
-import com.stdio.it_link_testapp.presentation.ui.components.CenteredColumn
 
 @Composable
 fun ImageItem(imageState: ImageData<String>, onRetry: () -> Unit) {
     when (imageState) {
         is LoadableData.Error -> {
-            CenteredColumn {
-                Icon(Icons.Default.Error, "Error")
-                OutlinedButton(modifier = Modifier.padding(top = 8.dp), onClick = onRetry) {
-                    Text(stringResource(R.string.retry))
+            Box {
+                Image(R.drawable.no_image)
+                Box(
+                    modifier = Modifier
+                        .padding(bottom = 8.dp)
+                        .align(Alignment.BottomCenter)
+                        .size(60.dp, 20.dp)
+                        .clickable(onClick = onRetry)
+                ) {
+                    Text(
+                        text = stringResource(R.string.retry),
+                        fontSize = 10.sp,
+                        color = MaterialTheme.colorScheme.primary,
+                        modifier = Modifier.align(Alignment.Center)
+                    )
                 }
             }
         }
