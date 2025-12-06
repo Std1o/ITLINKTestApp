6b6ca5e41b90ee6ad7010dc9f952e2776487a79b
Std1o <37378410+Std1o@users.noreply.github.com>
2025-12-06 17:42:19 +0300

    images optimization

--- app/src/main/java/com/stdio/it_link_testapp/presentation/viewmodel/ImageDetailViewModel.kt
+++ app/src/main/java/com/stdio/it_link_testapp/presentation/viewmodel/ImageDetailViewModel.kt
@@ -5,14 +5,17 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.stdio.it_link_testapp.domain.model.Image
 import com.stdio.it_link_testapp.domain.model.ImageData
-import com.stdio.it_link_testapp.domain.usecases.GetImagesUseCase
+import com.stdio.it_link_testapp.domain.model.LoadableData
+import com.stdio.it_link_testapp.domain.repository.ImageRepository
+import com.stdio.it_link_testapp.domain.usecases.GetRawImagesUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class ImageDetailViewModel @Inject constructor(
-    private val getImagesUseCase: GetImagesUseCase
+    private val repository: ImageRepository,
+    private val getRawImagesUseCase: GetRawImagesUseCase,
 ) : ViewModel() {
 
     private val _images = mutableStateListOf<ImageData<Image>>()
@@ -21,9 +24,14 @@ class ImageDetailViewModel @Inject constructor(
     init {
         viewModelScope.launch {
             try {
-                getImagesUseCase().collect { images ->
-                    _images.clear()
-                    _images.addAll(images)
+                _images.clear()
+                val rawImages = getRawImagesUseCase()
+                repeat(rawImages.size) { _images.add(LoadableData.Loading) }
+
+                rawImages.forEachIndexed { index, _ ->
+                    repository.loadImage(rawImages[index], index).collect {
+                        _images[index] = it
+                    }
                 }
             } catch (e: Exception) {
                 e.printStackTrace()
